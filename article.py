311b539107a8eeb256403090d1355d69c306c96c
TK <t100tk@hotmail.com>
2024-11-29 15:53:51 +0800

    2024年11月29日

--- article.py
+++ article.py
@@ -1,49 +1,37 @@
+from typing import Dict, List, Any, AnyStr
+
+import time
 import json
 import httpx
-
-from typing import Dict, Any, List
 
 
 def main():
 
     user_id: int = 36679026
     url: str = f"https://www.meipian.cn/service/user/{user_id}/article/open-list"
-    headers: dict[str, str] = {
+    headers: Dict[str, str] = {
         "User-Agent": "Opera/9.80 (iPhone; Opera Mini/8.0.0/34.2336; U; en) Presto/2.8.119 Version/11.10"
     }
-    params: dict[str, int] = {"last_mask_id": 0}
+    params: Dict[str, int] = {"last_mask_id": 0}
 
     with httpx.Client() as client:
         while True:
-            response: httpx.Response = client.get(
-                url=url, headers=headers, params=params
-            )
-            if response.status_code != 200:
-                raise Exception(f"请求失败")
             try:
-                data: Dict[str, List[Dict[str, Any]]] = response.json()
-            except json.JSONDecodeError:
-                raise Exception("无法解析响应数据")
-
-            page: List[Dict[str, any]] = data["data"]
-
-            items: list[dict[str, str]] = [
-                {
-                    "user_id": user_id,
-                    "mask_id": item["mask_id"],
-                    "create_time": item["create_time"],
-                    "title": item["title"].replace(" ", ""),
-                    "url": f"https://www.meipian.cn/{item['mask_id']}",
-                    "cover_img_url": item["cover_img_url"],
-                }
-                for item in page
-            ]
-            print(items)
-
-            params["last_mask_id"] = page[-1]["mask_id"]
-            if not page or len(page) < 20:
+                response: httpx.Response = client.get(
+                    url=url, headers=headers, params=params
+                )
+                data: Dict[str, List[Dict[str, str]]] = response.json()
+                page: List[Dict[str, str]] = data["data"]
+                if not page or len(page) > 20:
+                    break
+                params["last_mask_id"] = page[-1]["mask_id"]
+            except httpx.HTTPStatusError():
                 break
+            except json.JSONDecodeError():
+                break
+            finally:
+                time.sleep(0.5)
 
 
 if __name__ == "__main__":
-    main()
+    pass
